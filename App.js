7aec51906b588c63fb3a2d4cc3730d4d1f2de00e
VerneriLop <verneri.lopperi@gmail.com>
2023-04-25 10:34:28 +0300

    flatlist näkyvissä

--- App.js
+++ App.js
@@ -1,21 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import Main from './src/components/Main';
+import RepositoryList from './src/components/RepositoryList';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Button title="hei"></Button>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  <View>
+    <Main />
+    <RepositoryList/>
+  </View>
+  )
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
