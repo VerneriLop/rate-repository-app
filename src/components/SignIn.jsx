87dcd7f523e2b67a6eafa6c53e79fc6c61e2fd48
VerneriLop <verneri.lopperi@gmail.com>
2023-04-26 15:39:08 +0300

    signIn komponentti

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,7 +1,40 @@
+import { Formik } from 'formik';
+import { Pressable, View } from 'react-native';
+import FormikTextInput from './FormikTextInput';
 import Text from './Text';
 
+const initialValues = {
+    username: '',
+    password: '',
+};
+
+const SignInForm = ({onSubmit}) => {
+
+    return (
+        <View>
+          <FormikTextInput name="username" placeholder="Username" />
+          <FormikTextInput name="password" placeholder="Password" secureTextEntry/>
+          <Pressable onPress={onSubmit}>
+            <Text>Sign In</Text>
+          </Pressable>
+        </View>
+    );
+}
+
 const SignIn = () => {
-  return <Text>The sign-in view</Text>;
+
+    const onSubmit = (values) => {
+        console.log(values);
+    };
+
+    return (
+        <View>
+            <Text>The sign-in view</Text>
+            <Formik initialValues={initialValues} onSubmit={onSubmit}>
+                {({ handleSubmit }) => <SignInForm onSubmit={handleSubmit} />}
+            </Formik>
+        </View>
+    );
 };
 
 export default SignIn;
